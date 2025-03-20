c5fbbfa4f3dad2238275d79e90bad98d0cf1562a
savaged <savagedinfo@protonmail.com>
2025-03-20 11:23:48 +0000

    working

--- util/leaderboardtohtml.sh
+++ util/leaderboardtohtml.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-echo "<html><body><table>"
+output_file="html/index.html"
+
+echo "<html><body><table>" > "$output_file"
 
 # Read the header row
 header=$(head -n 1 leaderboard.csv)
 header=$(echo "$header" | sed 's/,/<\/th><th>/g')
-echo "<tr><th>$header</th></tr>"
+echo "<tr><th>$header</th></tr>" >> "$output_file"
 
 # Process the data rows
 tail -n +2 leaderboard.csv | while IFS=, read -r player total; do
-  echo "<tr><td>$player</td><td>$total</td></tr>"
+  echo "<tr><td>$player</td><td>$total</td></tr>" >> "$output_file"
 done
 
-echo "</table></body></html>"
+echo "</table></body></html>" >> "$output_file"
+

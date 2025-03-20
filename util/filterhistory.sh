874820c4fd5c2affc5e7cdac6ce5ad59b385b7fe
savaged <savagedinfo@protonmail.com>
2025-03-20 10:32:01 +0000

    working

---
+++ util/filterhistory.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+csvsql --query "$(cat util/filterhistory.sql)" smshistory.csv
+
---
+++ util/filterhistory.sql
@@ -0,0 +1,11 @@
+SELECT
+    date_added AS received,
+    CAST(`from` AS INTEGER) AS player,
+    SUBSTR(body, 1, INSTR(body, ' ') - 1) AS hole,
+    SUBSTR(body, INSTR(body, ' ') + 1, CASE WHEN INSTR(SUBSTR(body, INSTR(body, ' ') + 1), ' ') = 0 THEN LENGTH(body) ELSE INSTR(SUBSTR(body, INSTR(body, ' ') + 1), ' ') -1 END) AS gross,
+    SUBSTR(body, INSTR(body, ' ') + INSTR(SUBSTR(body, INSTR(body, ' ') + 1), ' ') + 1) AS points
+FROM
+    smshistory
+WHERE
+    direction = 'in'
+    AND body REGEXP '^[1-9]+ [1-9]+ [1-9]+$';

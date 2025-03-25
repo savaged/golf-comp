cba2c14624bf338f1810b48fd6f28f394270c798
savaged <savagedinfo@protonmail.com>
2025-03-25 10:41:45 +0000

    fix

--- src/filterhistory.sql
+++ src/filterhistory.sql
@@ -2,9 +2,19 @@ SELECT
     date_added AS received,
     CAST(`from` AS INTEGER) AS player,
     SUBSTR(body, 1, INSTR(body, ' ') - 1) AS hole,
-    SUBSTR(body, INSTR(body, ' ') + 1, CASE WHEN INSTR(SUBSTR(body, INSTR(body, ' ') + 1), ' ') = 0 THEN LENGTH(body) ELSE INSTR(SUBSTR(body, INSTR(body, ' ') + 1), ' ') -1 END) AS gross,
-    SUBSTR(body, INSTR(body, ' ') + INSTR(SUBSTR(body, INSTR(body, ' ') + 1), ' ') + 1) AS points
+    CASE
+        WHEN LENGTH(TRIM(body)) - LENGTH(REPLACE(body, ' ', '')) = 0 THEN 0  -- Handle single number case
+        ELSE SUBSTR(body, INSTR(body, ' ') + 1, CASE
+                                                     WHEN INSTR(SUBSTR(body, INSTR(body, ' ') + 1), ' ') = 0
+                                                     THEN LENGTH(body)
+                                                     ELSE INSTR(SUBSTR(body, INSTR(body, ' ') + 1), ' ') - 1
+                                                 END)
+    END AS gross,
+    CASE
+        WHEN LENGTH(TRIM(body)) - LENGTH(REPLACE(body, ' ', '')) < 2 THEN 0 --Handle one or zero spaces
+        ELSE SUBSTR(SUBSTR(body, INSTR(body, ' ') + 1), INSTR(SUBSTR(body, INSTR(body, ' ') + 1), ' ') + 1)
+    END AS points
 FROM smshistory
 WHERE direction = 'in'
 AND DATE(date_added) = DATE('now')
-AND body REGEXP '^[1-9]+ [1-9]+ [0-9]+$';
+AND body REGEXP '^[1-9]+\s[1-9]+(\s[0-9])*$';

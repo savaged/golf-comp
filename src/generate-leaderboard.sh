d95548e321535b1515a0c31f5855a56b1239bf71
savaged <savagedinfo@protonmail.com>
2025-03-25 14:45:25 +0000

    more quiet hopefully

--- src/generate-leaderboard.sh
+++ src/generate-leaderboard.sh
@@ -149,7 +149,7 @@ log_and_echo "$(date) - HTML file generated."
 
 log_and_echo "$(date) - Step 6: Deploy site"
 cd "$HTML_OUTPUT_DIR"
-ntl deploy --prod -d .
+ntl deploy --json --prod -d .
 cd "$BASE_DIR"
 
 log_and_echo "$(date) - All steps completed!"

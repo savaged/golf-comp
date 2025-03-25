0ad3c2d8918106f3307bd1591308d7b10f7c599a
savaged <savagedavid@gmail.com>
2025-03-25 17:16:26 +0000

    defensive

--- src/deploy-leaderboard.sh
+++ src/deploy-leaderboard.sh
@@ -9,19 +9,20 @@ NETLIFY_ENV_FILE="$BASE_DIR/.netlify_env"
 HTML_OUTPUT_DIR="$BASE_DIR/_site"
 LOG_FILE="$BASE_DIR/deploy-leaderboard.log"
 
-
-# A function to log and echo messages
 log_and_echo() {
   echo "$@" | tee -a "$LOG_FILE"
 }
 
-log_and_echo "$(date) - Starting script part 2..."
-
-
 log_and_echo "$(date) - Step 6: Deploy site"
+
+if [ ! -f "$NETLIFY_ENV_FILE" ]; then
+  log_and_echo "$(date) - ERROR: .netlify_env file not found at $NETLIFY_ENV_FILE"
+  exit 1
+fi
+
 source "$NETLIFY_ENV_FILE"
 cd "$HTML_OUTPUT_DIR"
-timeout 60 ntl deploy --prod -d . --silent 2>&1
+timeout 60 ntl deploy --prod -d . --json
 deploy_status=$?
 cd "$BASE_DIR"
 if [ $deploy_status -ne 0 ]; then
